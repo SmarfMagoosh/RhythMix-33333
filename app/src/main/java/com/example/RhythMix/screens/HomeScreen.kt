66d73fe315fd8d0d71208ec52538b5831e5ac057
avaHatfield <106991181+avaHatfield@users.noreply.github.com>
2023-11-14 15:15:28 -0500

    song list on home screen

--- app/src/main/java/com/example/RhythMix/screens/HomeScreen.kt
+++ app/src/main/java/com/example/RhythMix/screens/HomeScreen.kt
@@ -1,11 +1,64 @@
 package com.example.RhythMix.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.RhythMix.RhythMixViewModel
+import com.example.RhythMix.getSongs
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun HomeScreen(
     vm: RhythMixViewModel
 ) {
+//lazy column use items
+    //List songTitles = vm.getSongs()
+    LazyColumn(
+        content = { items(getSongs()) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .clickable {
+                       //TODO
+                    }
+            ) {
+                Text(it.title)
+            }
+
+
+        }
+
+        }
+    )
+
+//            Card(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(10.dp)
+//                    .clickable {
+//                       //TODO
+//                    }
+//            ) {
+//                Text("Song")
+//            }
+//        }
+    }
+
+
+@Composable
+fun SongRow(songTitle: String) {
+    Row(
+        modifier = Modifier
+    ){
+        Text(songTitle)
+    }
 
 }
