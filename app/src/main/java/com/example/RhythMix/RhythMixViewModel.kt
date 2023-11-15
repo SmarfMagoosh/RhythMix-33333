3ce7171bcc2bff85ac2818342ee7f6baf0d77386
Dreher <dreheren20@gcc.edu>
2023-11-15 17:19:07 -0500

    home screen lookin sick
    🔥🔥🔥🔥🔥🔥🔥

--- app/src/main/java/com/example/RhythMix/RhythMixViewModel.kt
+++ app/src/main/java/com/example/RhythMix/RhythMixViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.RhythMix
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.RhythMix.classes.Song
@@ -19,25 +22,26 @@ object Modifiers {
     val cardModifier: Modifier = Modifier
         .fillMaxWidth()
         .height(120.dp)
-        .padding(10.dp)
+        .padding(5.dp)
 }
-fun getSongs(): List<Song> = listOf(
-    Song("Reptilia", mutableListOf(), R.raw.reptilia),
-    Song("Yellow", mutableListOf(), R.raw.yellow),
-    Song("Black Betty", mutableListOf(), R.raw.black_betty),
-    Song("Mississippi Queen", mutableListOf(), R.raw.mississippi_queen),
-    Song("Head in the Ceiling Fan", mutableListOf(), R.raw.head_in_the_cieling_fan),
-    Song("Not the Same Anymore", mutableListOf(), R.raw.not_the_same_anymore)
-)
-fun getTracks(): List<Track> = listOf(
-    Track("The Sickest Riff Known to Mankind", R.raw.sweet_child_of_mine),
-    Track("Bass", R.raw.billie_jean),
-    Track("Vocals", R.raw.boulevard_of_broken_dreams),
-    Track("Saxophone Solo that Goes Crazy", R.raw.careless_whisper),
-    Track("Vine Boom Sound Effect", R.raw.vine_boom)
-)
 class RhythMixViewModel: ViewModel() {
     private var _state = MutableStateFlow( State() )
     val state = _state.asStateFlow()
     // TODO
+
+    fun getSongs(): List<Song> = listOf(
+        Song("Reptilia", mutableListOf(), R.raw.reptilia),
+        Song("Yellow", mutableListOf(), R.raw.yellow),
+        Song("Black Betty", mutableListOf(), R.raw.black_betty),
+        Song("Mississippi Queen", mutableListOf(), R.raw.mississippi_queen),
+        Song("Head in the Ceiling Fan", mutableListOf(), R.raw.head_in_the_cieling_fan),
+        Song("Not the Same Anymore", mutableListOf(), R.raw.not_the_same_anymore)
+    )
+    fun getTracks(): List<Track> = listOf(
+        Track("The Sickest Riff Known to Mankind", R.raw.sweet_child_of_mine),
+        Track("Bass", R.raw.billie_jean),
+        Track("Vocals", R.raw.boulevard_of_broken_dreams),
+        Track("Saxophone Solo that Goes Crazy", R.raw.careless_whisper),
+        Track("Vine Boom Sound Effect", R.raw.vine_boom)
+    )
 }
